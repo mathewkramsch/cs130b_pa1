9961ea231a573f612c016c1df420b138aa78ede6
Mathew Kramsch <mathewkramsch@ucsb.edu>
2020-04-24 15:45:26 -0700

    solved elevator problem, passes test cases on kattis

--- elevator.cpp
+++ elevator.cpp
@@ -4,35 +4,34 @@
 #include "string"
 using namespace std;
 
-int main(int argc, char *argv[]) {
-	int const numFloors = argv[1];  // total number of floors
-	int current = argv[2];  // current floor
-	int const goal = argv[3];  // floor to reach
-	int const up = argv[4];  // number floors up when press UP
-	int const down = argv[5];  // number floors down when press DOWN
-	int count  = 0;  // number of pushes used
+int main() {
+	int numFloors, current, goal, up, down;
+	cin >> numFloors >> current >> goal >> up >> down;
+	int count = 0;  // number of pushes used
 	string mssg = "use the stairs\n";
 
-	if (current == goal) cout << mssg;
-	else if (current > goal) {
-		while (current > goal) {
+	if (current > goal) {
+		while (current>goal && down!=0) {
 			current -= down;
 			count++;
 		}
-		if (current != goal) {  // then overshot, go one up
-			// do some shit
-		}
-	} 
-	else {
-		while (current < goal) {
+		while (current<goal && up!=0) {
 			current += up;
 			count++;
 		}
-		if (current != goal) {
+	} else {
+		while (current<goal && up!=0) {
+			current += up;
+			count++;
+		}
+		while (current>goal && down!=0) {
+			current -= down;
+			count++;
 		}
 	}
 
-	cout << count << endl;
+	if (current == goal) cout << count << endl;
+	else cout << mssg;
 	return 0;
 }
 
